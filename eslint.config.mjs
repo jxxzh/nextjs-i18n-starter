5af475480d578ec98d7b8d7450256c4a268a34e2
huxichun <huxichun163@163.com>
2024-11-14 13:51:57 +0800

    feat: 导航组件优先使用next-intl包裹的

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,19 @@ export default antfu({
   react: true,
   rules: {
     'unused-imports/no-unused-imports': 'warn',
+    'n/prefer-global/process': 'off',
+    // 导航优先使用 next-intl 包裹的方法
+    'no-restricted-imports': [
+      'warn',
+      {
+        name: 'next/link',
+        message: 'Please import from `@/lib/i18n/routing` instead.',
+      },
+      {
+        name: 'next/navigation',
+        importNames: ['redirect', 'permanentRedirect', 'useRouter', 'usePathname'],
+        message: 'Please import from `@/lib/i18n/routing` instead.',
+      },
+    ],
   },
 })
--- lib/i18n/routing.ts
+++ lib/i18n/routing.ts
@@ -23,5 +23,5 @@ export const routing = defineRouting({
 
 // Lightweight wrappers around Next.js' navigation APIs
 // that will consider the routing configuration
-export const { Link, redirect, usePathname, useRouter }
+export const { Link, redirect, permanentRedirect, usePathname, useRouter }
   = createNavigation(routing)
